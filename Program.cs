cd84442781e450f86818b5abb711bf48ef94ccd3
SUCCESSIVE\akshat.kumar <akshat.kumar@successive.tech>
2023-10-25 17:36:40 +0530

    First C# program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ThisIsMyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello There!");
+
+            int a = 78;
+
+            Console.WriteLine("The value of a is: " + a);
+        }
+    }
+}
